154b722b58f2056f4423b9813051adc30615e52c
seoeungi <sjlim02255@gmail.com>
2025-11-10 01:54:53 +0900

    java 버전 변경

--- src/main/java/seg/work/geuliumieum/server/GeuliumIeumApplication.java
+++ src/main/java/seg/work/geuliumieum/server/GeuliumIeumApplication.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 public class GeuliumIeumApplication {
 
-    static void main(String[] args) {
+    public static void main(String[] args) {
         SpringApplication.run(GeuliumIeumApplication.class, args);
     }
 
