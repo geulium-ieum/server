d2ca353008e37b69017ef784b9a14c9bbda2cdc6
seoeungi <sjlim02255@gmail.com>
2025-11-23 22:22:47 +0900

    AuditorConfig 수정

--- src/main/java/seg/work/geuliumieum/server/config/jpa/AuditorConfig.java
+++ src/main/java/seg/work/geuliumieum/server/config/jpa/AuditorConfig.java
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import seg.work.geuliumieum.server.common.dto.UserInfo;
+import seg.work.geuliumieum.server.common.exception.ApiException;
+import seg.work.geuliumieum.server.common.exception.ErrorCode;
 import seg.work.geuliumieum.server.config.security.CustomUserDetails;
 
 @Configuration
@@ -14,14 +17,16 @@ public class AuditorConfig implements AuditorAware<Long> {
     public Optional<Long> getCurrentAuditor() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if (authentication == null || !authentication.isAuthenticated()) {
+            throw new ApiException(ErrorCode.USER_NOT_FOUND);
+        } else {
+            Object principal = authentication.getPrincipal();
+            if (principal instanceof UserInfo userInfo) {
+                return Optional.ofNullable(userInfo.getId());
+            } else if (principal instanceof CustomUserDetails cud) {
+                return Optional.ofNullable(cud.getId());
+            }
+
             return Optional.empty();
         }
-
-        Object principal = authentication.getPrincipal();
-        if (principal instanceof CustomUserDetails cud) {
-            return Optional.ofNullable(cud.getId());
-        }
-
-        return Optional.empty();
     }
 }
