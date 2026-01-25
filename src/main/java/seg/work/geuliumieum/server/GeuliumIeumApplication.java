31d26fd79055d7ce933e4d2d670148d06d862133
seoeungi <sjlim02255@gmail.com>
2026-01-25 22:57:38 +0900

    - 가상 스레드 활성화 - redis repository 비활성화 - 중복 의존성 제거 - jpa repository 범위 지정

--- src/main/java/seg/work/geuliumieum/server/GeuliumIeumApplication.java
+++ src/main/java/seg/work/geuliumieum/server/GeuliumIeumApplication.java
@@ -4,6 +4,7 @@ import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -12,6 +13,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @SpringBootApplication
 @EnableSpringDataWebSupport(pageSerializationMode = PageSerializationMode.VIA_DTO)
+@EnableJpaRepositories(basePackages = "seg.work.geuliumieum.server.common.repository")
 public class GeuliumIeumApplication {
 
     public static void main(String[] args) {
